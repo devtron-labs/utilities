8e686bab25e53f6ead88005f0e8f1a14d209abe9
Prakarsh <71125043+prakarsh-dt@users.noreply.github.com>
2022-09-13 21:39:58 +0530

    Create microk8s-with-devtron.sh

---
+++ microk8s/microk8s-with-devtron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Installing microk8s over ubuntu 18
+sudo snap install microk8s --classic --channel=1.22
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+newgrp microk8s
+microk8s enable dns storage helm3 ingress
+echo "alias kubectl='microk8s kubectl '" >> .bashrc
+echo "alias helm='microk8s helm3 '" >> .bashrc
+source .bashrc
+kubectl create ns devtroncd
+
+#Installing Devtron
+helm repo add devtron https://helm.devtron.ai
+helm install devtron devtron/devtron-operator --create-namespace --namespace devtroncd  --set components.devtron.service.type=ClusterIP --set components.devtron.ingress.enabled=true --set components.devtron.ingress.className=public --set components.devtron.ingress.host="devtron.example.com"
