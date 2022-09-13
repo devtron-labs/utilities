7757cfc5e447fa29d8c94be701c5749d400eb8bd
Prakarsh <71125043+prakarsh-dt@users.noreply.github.com>
2022-09-13 22:39:09 +0530

    Update microk8s-with-devtron.sh

--- microk8s/microk8s-with-devtron.sh
+++ microk8s/microk8s-with-devtron.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #Installing microk8s over ubuntu 18
-sudo snap install microk8s --classic --channel=1.22 && \
-sudo usermod -a -G microk8s $USER && \
-sudo chown -f -R $USER ~/.kube && \
-newgrp microk8s && \
-microk8s enable dns storage helm3 ingress && \
+sudo snap install microk8s --classic --channel=1.22 && sudo usermod -a -G microk8s $USER && sudo chown -f -R $USER ~/.kube && newgrp microk8s && microk8s enable dns storage helm3 ingress && \
 echo "alias kubectl='microk8s kubectl '" >> .bashrc && \
 echo "alias helm='microk8s helm3 '" >> .bashrc && \
 source .bashrc && \
