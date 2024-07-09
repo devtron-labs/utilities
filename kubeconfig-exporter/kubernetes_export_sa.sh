d35cde514196704ab0680fdb08c97fdd87b0c532
Neha Sharma <nehasharma@Nehas-MacBook-Pro.local>
2024-07-09 13:21:50 +0530

    changes in the script

--- kubeconfig-exporter/kubernetes_export_sa.sh
+++ kubeconfig-exporter/kubernetes_export_sa.sh
@@ -17,8 +17,8 @@ SERVER_URL=""
 TOKEN=""
 
 create_cluster_role_binding(){
-   echo -e "\\nCreating cluster role binding of name ${SERVICE_ACCOUNT_NAME} with clusterRole cluster-admin"
-    kubectl apply -f - <<EOF
+   echo -e "\\nCreating cluster role binding of name ${SERVICE_ACCOUNT_NAME} with clusterRole cluster-admin" 
+   kubectl apply -f - <<EOF
 apiVersion: v1
 kind: Namespace
 metadata:
@@ -48,18 +48,18 @@ create_target_folder() {
 
 create_service_account() {
     echo -e "\\nCreating a service account in ${NAMESPACE} namespace: ${SERVICE_ACCOUNT_NAME}"
-     kubectl create sa "${SERVICE_ACCOUNT_NAME}" --namespace "${NAMESPACE}"
+    kubectl create sa "${SERVICE_ACCOUNT_NAME}" --namespace "${NAMESPACE}"
 }
 
 create_serviceaccount_token(){
     echo -e "\\nCreating service account token in ${NAMESPACE} namespace: ${SERVICE_ACCOUNT_NAME}\n"
 
-    TOKEN=$( kubectl create token "${SERVICE_ACCOUNT_NAME}" -n "${NAMESPACE}")
+    TOKEN=$(kubectl create token "${SERVICE_ACCOUNT_NAME}" -n "${NAMESPACE}")
 }
 
 create_secret(){
     echo -e "creating secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-     kubectl apply -n "${NAMESPACE}" -f - <<EOF
+    kubectl apply -n "${NAMESPACE}" -f - <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
@@ -73,50 +73,50 @@ echo "secret created"
 
 get_secret_name_from_secret() {
     echo -e "\\nGetting secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-    SECRET_NAME=$( kubectl get secret "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o=jsonpath={.metadata.name})
+    SECRET_NAME=$(kubectl get secret "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o=jsonpath={.metadata.name})
     echo "Secret name: ${SECRET_NAME}"
  }
 
 get_secret_name_from_service_account() {
     echo -e "\\nGetting secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-    SECRET_NAME=$( kubectl get sa "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o=jsonpath='{.secrets[*].name}')
+    SECRET_NAME=$(kubectl get sa "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o=jsonpath='{.secrets[*].name}')
     echo "Secret name: ${SECRET_NAME}"
 }
 
 extract_ca_crt_from_secret() {
     echo -e -n "\\nExtracting ca.crt from secret..."
-     kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o=jsonpath="{.data.ca\.crt}"| base64 --decode > "${TARGET_FOLDER}/ca.crt"
+    kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o=jsonpath="{.data.ca\.crt}"| base64 --decode > "${TARGET_FOLDER}/ca.crt"
     printf "done"
 }
 
 get_user_token_from_secret() {
     echo -e -n "\\nGetting user token from secret..."
-    TOKEN=$( kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o=jsonpath={.data.token}|base64 --decode)
+    TOKEN=$(kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o=jsonpath={.data.token}|base64 --decode)
     printf "done"
 }
 
 set_kube_config_values() {
-    context=$( kubectl config current-context)
+    context=$(kubectl config current-context)
     echo -e "\\nSetting current context to: $context"
 
-    CLUSTER_NAME=$( kubectl config get-contexts "$context" | awk '{print $3}' | tail -n 1)
+    CLUSTER_NAME=$(kubectl config get-contexts "$context" | awk '{print $3}' | tail -n 1)
     echo "Cluster name: ${CLUSTER_NAME}"
 
-   SERVER_URL=$( kubectl config view \
+   SERVER_URL=$(kubectl config view \
     -o jsonpath="{.clusters[?(@.name == \"${CLUSTER_NAME}\")].cluster.server}")
 
 
     # Set up the config
     echo -e "\\nPreparing k8s-${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-conf"
     echo -n "Setting a cluster entry in kubeconfig..."
-     kubectl config set-cluster "${CLUSTER_NAME}" \
+    kubectl config set-cluster "${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --server="${SERVER_URL}" \
     --certificate-authority="${TARGET_FOLDER}/ca.crt" \
     --embed-certs=true
 
     echo -n "Setting token credentials entry in kubeconfig..."
-     kubectl config set-credentials \
+    kubectl config set-credentials \
     "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --token="${TOKEN}"
@@ -130,25 +130,21 @@ set_kube_config_values() {
     --namespace="${NAMESPACE}"
 
     echo -n "Setting the current-context in the kubeconfig file..."
-     kubectl config use-context "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
+    kubectl config use-context "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}"
 }
 
 
-#CLIENT_VERSION=$( kubectl version --client | awk '/Client Version: /{print $3}'| cut -d '.' -f 2)
-# CLIENT_VERSION=$( kubectl version -o json | awk -F '"' '/"clientVersion"/ {getline; getline; print $4}' | cut -d '.' -f 2 )
+#CLIENT_VERSION=$(kubectl version --client | awk '/Client Version: /{print $3}'| cut -d '.' -f 2)
 CLIENT_VERSION=$( kubectl version -o json | awk -F '"' '/"clientVersion"/ {getline; getline; print $4}' | cut -d '.' -f 2)
-
 echo "$CLIENT_VERSION"
 if [[ $CLIENT_VERSION -gt 27 ]]
-then
-    #VERSION=$( kubectl version | awk '/Server Version: /{print $3}' | cut -d '.' -f 2 )
+then 
+    #VERSION=$(kubectl version | awk '/Server Version: /{print $3}' | cut -d '.' -f 2 )
     VERSION=$(kubectl version -o json | awk -F '"' '/"serverVersion"/ {getline; getline; print $4}' | cut -d '.' -f 2    )
-    VERSION=$(expr $VERSION )
-    
+    VERSION=$(expr $VERSION)
 else
     VERSION=$(kubectl version --short | awk '/Server Version: /{print $3}' | cut -d '.' -f 2 )
-
     VERSION=$(expr $VERSION)
 fi
 
@@ -170,10 +166,10 @@ else
  extract_ca_crt_from_secret
  get_user_token_from_secret
  set_kube_config_values
-fi
+fi 
 
 echo -e "\\nAll done! Test with:"
-echo "KUBECONFIG=${KUBECFG_FILE_NAME}  kubectl get pods"
+echo "KUBECONFIG=${KUBECFG_FILE_NAME} kubectl get pods"
 echo "you should not have any permissions by default - you have just created the authentication part"
 echo "You will need to create RBAC permissions"
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
@@ -182,4 +178,4 @@ echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 echo "BEARER TOKEN := ${TOKEN} "
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
 
-KUBECONFIG=${KUBECFG_FILE_NAME}  kubectl get pods
+KUBECONFIG=${KUBECFG_FILE_NAME} kubectl get pods
