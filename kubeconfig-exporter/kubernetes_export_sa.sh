1223632e2625ea9592fea3d8c39f954810161c39
Neha Sharma <156081591+neha130@users.noreply.github.com>
2025-03-19 12:07:33 +0530

    Update kubernetes_export_sa.sh

--- kubeconfig-exporter/kubernetes_export_sa.sh
+++ kubeconfig-exporter/kubernetes_export_sa.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 if [[ $# -lt 2 ]]; then
   echo "usage: $0 <service_account_name> <namespace> [--devtron-endpoint=<value>] [--devtron-api-token=<value>] [--cluster-name=<value>] [--insecure=<value>] [--server_url=<value>]"
-  echo "ex: sh ./kubeconfig-exporter/kubernetes_export_sa.sh cd-user cd-user --cluster-name=cluster-name"
+  echo "ex: sh ./kubeconfig-exporter/kubernetes_export_sa.sh cd-user devtroncd"
   exit 1
 fi
 
@@ -51,7 +51,7 @@ TOKEN=""
 
 create_cluster_role_binding(){
    echo -e "\\nCreating cluster role binding of name ${SERVICE_ACCOUNT_NAME} with clusterRole cluster-admin"
-    kubectl apply -f - <<EOF
+    microk8s kubectl apply -f - <<EOF
 apiVersion: v1
 kind: Namespace
 metadata:
@@ -81,17 +81,17 @@ create_target_folder() {
 
 create_service_account() {
     echo -e "\\nCreating a service account in ${NAMESPACE} namespace: ${SERVICE_ACCOUNT_NAME}"
-     kubectl create sa "${SERVICE_ACCOUNT_NAME}" --namespace "${NAMESPACE}"
+     microk8s kubectl create sa "${SERVICE_ACCOUNT_NAME}" --namespace "${NAMESPACE}"
 }
 
 create_serviceaccount_token(){
     echo -e "\\nCreating service account token in ${NAMESPACE} namespace: ${SERVICE_ACCOUNT_NAME}\n"
-    TOKEN=$( kubectl create token "${SERVICE_ACCOUNT_NAME}" -n "${NAMESPACE}")
+    TOKEN=$( microk8s kubectl create token "${SERVICE_ACCOUNT_NAME}" -n "${NAMESPACE}")
 }
 
 create_secret(){
     echo -e "creating secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-     kubectl apply -n "${NAMESPACE}" -f - <<EOF
+     microk8s kubectl apply -n "${NAMESPACE}" -f - <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
@@ -105,55 +105,55 @@ echo "secret created"
 
 get_secret_name_from_secret() {
     echo -e "\\nGetting secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-    SECRET_NAME=$( kubectl get secret "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o=jsonpath={.metadata.name})
+    SECRET_NAME=$( microk8s kubectl get secret "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o=jsonpath={.metadata.name})
     echo "Secret name: ${SECRET_NAME}"
 }
 
 get_secret_name_from_service_account() {
     echo -e "\\nGetting secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-    SECRET_NAME=$( kubectl get sa "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o=jsonpath='{.secrets[*].name}')
+    SECRET_NAME=$( microk8s kubectl get sa "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o=jsonpath='{.secrets[*].name}')
     echo "Secret name: ${SECRET_NAME}"
 }
 
 extract_ca_crt_from_secret() {
     echo -e -n "\\nExtracting ca.crt from secret..."
-     kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o=jsonpath="{.data.ca\.crt}"| base64 --decode > "${TARGET_FOLDER}/ca.crt"
+     microk8s kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o=jsonpath="{.data.ca\.crt}"| base64 --decode > "${TARGET_FOLDER}/ca.crt"
     printf "done"
 }
 
 get_user_token_from_secret() {
     echo -e -n "\\nGetting user token from secret..."
-    TOKEN=$( kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o=jsonpath={.data.token}|base64 --decode)
+    TOKEN=$( microk8s kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o=jsonpath={.data.token}|base64 --decode)
     printf "done"
 }
 
 set_kube_config_values() {
-    context=$( kubectl config current-context)
+    context=$( microk8s kubectl config current-context)
     echo -e "\\nSetting current context to: $context"
 
-    CLUSTER_NAME=$( kubectl config get-contexts "$context" | awk '{print $3}' | tail -n 1)
+    CLUSTER_NAME=$( microk8s kubectl config get-contexts "$context" | awk '{print $3}' | tail -n 1)
     echo "Cluster name: ${CLUSTER_NAME}"
 
-    SERVER_URL=$( kubectl config view \
+    SERVER_URL=$( microk8s kubectl config view \
     -o jsonpath="{.clusters[?(@.name == \"${CLUSTER_NAME}\")].cluster.server}")
 
     # Set up the config
     echo -e "\\nPreparing k8s-${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-conf"
     echo -n "Setting a cluster entry in kubeconfig..."
-     kubectl config set-cluster "${CLUSTER_NAME}" \
+     microk8s kubectl config set-cluster "${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --server="${SERVER_URL}" \
     --certificate-authority="${TARGET_FOLDER}/ca.crt" \
     --embed-certs=true
 
     echo -n "Setting token credentials entry in kubeconfig..."
-     kubectl config set-credentials \
+     microk8s kubectl config set-credentials \
     "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --token="${TOKEN}"
 
     echo -n "Setting a context entry in kubeconfig..."
-     kubectl config set-context \
+     microk8s kubectl config set-context \
     "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --cluster="${CLUSTER_NAME}" \
@@ -161,18 +161,19 @@ set_kube_config_values() {
     --namespace="${NAMESPACE}"
 
     echo -n "Setting the current-context in the kubeconfig file..."
-     kubectl config use-context "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
+     microk8s kubectl config use-context "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}"
 }
 
-CLIENT_VERSION=$( kubectl version -o json | awk -F '"' '/"clientVersion"/ {getline; getline; print $4}' | cut -d '.' -f 2)
+CLIENT_VERSION=$(microk8s kubectl version -o json | awk -F '"' '/"clientVersion"/ {getline; getline; print $4}' | cut -d '.' -f 2)
+CLIENT_VERSION=$(echo "${CLIENT_VERSION%+}")
 echo "$CLIENT_VERSION"
 if [[ $CLIENT_VERSION -gt 27 ]]
 then
-    VERSION=$( kubectl version | awk '/Server Version: /{print $3}' | cut -d '.' -f 2 )
+    VERSION=$( microk8s kubectl version | awk '/Server Version: /{print $3}' | cut -d '.' -f 2 )
     VERSION=$(expr $VERSION)
 else
-    VERSION=$( kubectl version --short | awk '/Server Version: /{print $3}' | cut -d '.' -f 2 )
+    VERSION=$( microk8s kubectl version --short | awk '/Server Version: /{print $3}' | cut -d '.' -f 2 )
     VERSION=$(expr $VERSION)
 fi
 
@@ -197,7 +198,7 @@ else
 fi
 
 echo -e "\\nAll done! Test with:"
-echo "KUBECONFIG=${KUBECFG_FILE_NAME} kubectl get pods"
+echo "KUBECONFIG=${KUBECFG_FILE_NAME} microk8s kubectl get pods"
 echo "you should not have any permissions by default - you have just created the authentication part"
 echo "You will need to create RBAC permissions"
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
@@ -206,7 +207,7 @@ echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 echo "BEARER TOKEN := ${TOKEN} "
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
 
-KUBECONFIG=${KUBECFG_FILE_NAME} kubectl get pods
+KUBECONFIG=${KUBECFG_FILE_NAME} microk8s kubectl get pods
 
 # Handle server URL override if provided
 if [[ ! -z "$server_url" ]]; then
@@ -216,18 +217,48 @@ else
 fi
 
 # Add cluster to Devtron if all required parameters are provided
+# if [[ ! -z "${devtron_endpoint}" ]] && [[ ! -z "${devtron_api_token}" ]] && [[ ! -z "$cluster_name" ]]; then
+#     json_data=$(jq -n --argjson id null \
+#         --arg cluster_name "$cluster_name" \
+#         --arg bearerToken "$TOKEN" \
+#         --arg server_url "${SERVER_URL}" \
+#         '{id: $id, insecureSkipTlsVerify: true, cluster_name: $cluster_name, config: {bearer_token: $bearerToken}, active: true, "remoteConnectionConfig": {"connectionMethod": "DIRECT", "proxyConfig": null, "sshConfig": null}, "prometheus_url": "", "prometheusAuth": {"userName": "", "password": "", "tlsClientKey": "", "tlsClientCert": "", "isAnonymous": true}, server_url: $server_url}')
+
+#     res=$(curl -k "${devtron_endpoint}/orchestrator/cluster" \
+#         -H "Content-Type: application/json" \
+#         -H "token: $devtron_api_token" \
+#         --data-raw "$json_data")
+
 if [[ ! -z "${devtron_endpoint}" ]] && [[ ! -z "${devtron_api_token}" ]] && [[ ! -z "$cluster_name" ]]; then
-    json_data=$(jq -n --argjson id null \
-        --arg cluster_name "$cluster_name" \
-        --arg bearerToken "$TOKEN" \
-        --arg server_url "${SERVER_URL}" \
-        '{id: $id, insecureSkipTlsVerify: true, cluster_name: $cluster_name, config: {bearer_token: $bearerToken}, active: true, "remoteConnectionConfig": {"connectionMethod": "DIRECT", "proxyConfig": null, "sshConfig": null}, "prometheus_url": "", "prometheusAuth": {"userName": "", "password": "", "tlsClientKey": "", "tlsClientCert": "", "isAnonymous": true}, server_url: $server_url}')
+
+    json_data='{
+      "id": null,
+      "insecureSkipTlsVerify": true,
+      "cluster_name": "'"${cluster_name}"'",
+      "config": {
+        "bearer_token": "'"${TOKEN}"'"
+      },
+      "active": true,
+      "remoteConnectionConfig": {
+        "connectionMethod": "DIRECT",
+        "proxyConfig": null,
+        "sshConfig": null
+      },
+      "prometheus_url": "",
+      "prometheusAuth": {
+        "userName": "",
+        "password": "",
+        "tlsClientKey": "",
+        "tlsClientCert": "",
+        "isAnonymous": true
+      },
+      "server_url": "'"${SERVER_URL}"'"
+    }'
 
     res=$(curl -k "${devtron_endpoint}/orchestrator/cluster" \
         -H "Content-Type: application/json" \
         -H "token: $devtron_api_token" \
         --data-raw "$json_data")
-
 
     echo "Cluster Name: $cluster_name"
     echo "API Response: $res"
@@ -239,6 +270,6 @@ elif ([ ! -z "${devtron_endpoint}" ] && [  -z "${devtron_api_token}" ] && [  -z 
      ([ ! -z "${devtron_endpoint}" ] && [  -z "${devtron_api_token}" ] && [ ! -z "$cluster_name" ]) || \
      ([  -z "${devtron_endpoint}" ] && [ ! -z "${devtron_api_token}" ] && [ ! -z "$cluster_name" ]); then
 
-    echo "provide all the input to create cluster on devtron"\
+    echo "Please provide all required inputs to add cluster on devtron"\
 
 fi
