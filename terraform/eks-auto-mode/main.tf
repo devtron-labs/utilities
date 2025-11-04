108fa0075dc22bf2d927a61d558d3ab583eb33d1
kamal-devtron <128121299+kamal-devtron@users.noreply.github.com>
2025-11-04 17:45:14 +0530

    Update main.tf

--- terraform/eks-auto-mode/main.tf
+++ terraform/eks-auto-mode/main.tf
@@ -35,7 +35,7 @@ data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_name
 }
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.eks.token
